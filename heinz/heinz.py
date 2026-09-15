404a4e0bb1782cdd293dff620ecbfc92e905947b
Vivek Booshan <69310398+vivek-booshan@users.noreply.github.com>
2026-09-15 11:47:36 -0400

    Create heinz.py
    Add files via upload
    
    Rename dense_vs_davidson_overlay_loglog.pdf to example1.pdf
    
    Rename optimal_configuration_pareto.pdf to example2.pdf
    
    Create heinz.py
    
    move into heinz

---
+++ heinz/heinz.py
@@ -0,0 +1,38 @@
+PLOT_RC = {
+    # Figure & Layout
+    "figure.figsize": (3.4, 3.4), # if 2 panel plot (3.4, 3.4*1.5)
+    "figure.autolayout": True,
+    "figure.subplot.top": 0.99,
+    "figure.subplot.right": 0.99,
+    "savefig.dpi": 600,
+    # Font & Text
+    "font.family": "sans-serif",
+    "axes.labelsize": 10,
+    "xtick.labelsize": 8,
+    "ytick.labelsize": 8,
+    # Lines & Markers
+    "lines.linewidth": 2.0,
+    "lines.markersize": 7.0,
+    # Ticks
+    "xtick.direction": "in",
+    "ytick.direction": "in",
+    "xtick.minor.visible": True,
+    "ytick.minor.visible": True,
+    "xtick.bottom": True,
+    "ytick.left": True,
+    "xtick.major.size": 4.0,
+    "ytick.major.size": 4.0,
+    "xtick.minor.size": 2.0,
+    "ytick.minor.size": 2.0,
+    "xtick.major.width": 0.8,
+    "ytick.major.width": 0.8,
+    # Legend
+    "legend.frameon": True,
+    "legend.fontsize": 8,
+    "legend.title_fontsize": 9,
+    # Grid
+    "axes.grid": True,
+    "grid.alpha": 0.5,
+    "grid.linestyle": "--",
+}
+
